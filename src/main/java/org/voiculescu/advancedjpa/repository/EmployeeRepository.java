31413e9d31242d777758f12519c7d3a0098013c7
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2023-05-25 09:39:50 +0300

    Step44-45: Inheritance Hierarchies - Single Table PRO: Performance (all data in a select) AGAINST: Possibility of bad data

---
+++ src/main/java/org/voiculescu/advancedjpa/repository/EmployeeRepository.java
@@ -0,0 +1,33 @@
+package org.voiculescu.advancedjpa.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.voiculescu.advancedjpa.entity.Employee;
+
+import java.util.List;
+
+@Slf4j
+@Repository
+@Transactional
+public class EmployeeRepository {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public EmployeeRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Employee save(Employee employee){
+        entityManager.persist(employee);
+        return employee;
+    }
+
+    public List<Employee> retrieveAll(){
+        return entityManager.createQuery("select e from Employee e",Employee.class).getResultList();
+    }
+
+}
