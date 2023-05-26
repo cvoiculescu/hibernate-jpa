b6b6e46b833cb17cb3e319c7d1306faa1e15d380
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2023-05-26 15:48:57 +0300

    Step55: CriteriaQuery - retrieveAllCourse like 50 steps (where condition - predicates)

--- src/test/java/org/voiculescu/advancedjpa/repository/CriteriaQueryTest.java
+++ src/test/java/org/voiculescu/advancedjpa/repository/CriteriaQueryTest.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,14 @@ import org.voiculescu.advancedjpa.entity.Course;
 
 import java.util.List;
 
+/**
+ * "select c from Course c"
+ * 1. Use CriteriaBuilder to create a criteria query
+ * 2. Define root tables involved
+ * 3. Define Predicates
+ * 4. Add predicates
+ * 5. Build TypedQuery
+ */
 @Slf4j
 @SpringBootTest(classes = AdvancedJpaApplication.class)
 class CriteriaQueryTest {
@@ -22,17 +31,8 @@ class CriteriaQueryTest {
     EntityManager em;
 
     @Test
-    public void jpql_basic() {
-        /* "select c from Course c"
-            1. Use CriteriaBuilder to create a criteria query
-            2. Define root tables involved
-            3. Define Predicates
-            4. Add predicates
-            5. Build TypedQuery
-         */
-
-
-        //* -------- Remove one / for other behaviour
+    public void criteria_builder_all_course() {
+        //*
 
         //1
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -43,7 +43,6 @@ class CriteriaQueryTest {
         TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
 
         /*/
-        // ------- DO NOT REMOVE THIS UPPER COMMENT
 
         TypedQuery<Course> query = em.createQuery("select c from Course c", Course.class);
 
@@ -52,7 +51,28 @@ class CriteriaQueryTest {
 
         List<Course> resultList = query.getResultList();
         log.info("TypedQuery -> {}", resultList);
+    }
 
+    @Test
+    public void criteria_builder_all_course_like() {
+        //*
+        // 1
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+        // 2
+        Root<Course> courseRoot = cq.from(Course.class);
+        // 3,4 define predicates and add them
+        Predicate nameLike50steps = cb.like(courseRoot.get("name"), "%50%");
+        cq.where(nameLike50steps);
+        // 5
+        TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+
+        /*/
+        TypedQuery<Course> query = em.createQuery("select c from Course c where c.name like '%50%'", Course.class);
+        //*/
+
+        List<Course> resultList = query.getResultList();
+        log.info("TypedQuery -> {}", resultList);
     }
 
 }
