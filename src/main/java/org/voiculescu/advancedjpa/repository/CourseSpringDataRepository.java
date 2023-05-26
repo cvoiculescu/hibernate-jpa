58cd7ed60181e370ac6107e2a630892bbd266788
Corneliu Voiculescu <corneliu@voiculescu.org>
2023-05-26 22:57:36 +0300

    Step64: Added CoureSpringDataRepository

---
+++ src/main/java/org/voiculescu/advancedjpa/repository/CourseSpringDataRepository.java
@@ -0,0 +1,9 @@
+package org.voiculescu.advancedjpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.voiculescu.advancedjpa.entity.Course;
+
+@Repository
+public interface CourseSpringDataRepository extends JpaRepository<Course,Long> {
+}
